21d88001b6ea12276c01ef01d034352094cff0ce
riversry <94756266+KianChilds@users.noreply.github.com>
2024-11-23 12:14:14 -0600

    auto_pentagon

--- src/main/java/frc/robot/commands/AutonomousPentagon.java
+++ src/main/java/frc/robot/commands/AutonomousPentagon.java
@@ -3,21 +3,21 @@ package frc.robot.commands;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class AutonomousTriangle extends SequentialCommandGroup {
+public class AutonomousPentagon extends SequentialCommandGroup {
   /**
    * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
    * turn around and drive back.
    *
    * @param drivetrain The drivetrain subsystem on which this command will run
    */
-  public AutonomousTriangle(Drivetrain drivetrain) {
-    for(int count = 0; count<3; count++)
+  public AutonomousPentagon(Drivetrain drivetrain) {
+    for(int count = 0; count<5; count++)
     addCommands(
         new DriveDistance(0.5, 10, drivetrain),
-        new TurnDegrees(0.5, 80, drivetrain),
+        new TurnDegrees(0.5, 108, drivetrain));
   }
 
-  public AutonomousTriangle something(){
+  public AutonomousPentagon something(){
     return this;
   }
 }
